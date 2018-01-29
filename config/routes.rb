c6ac7ed7d643f282922da8a5d76ea7467105aebb
David Allen <davideallen7@gmail.com>
2018-01-29 13:36:16 -0800

    updated routes and defined root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  
+  #define root url
+  root 'pages#index'
+
+  #define routes for pages
+  get '/home' => 'pages#home'
+  get '/profile' => 'pages#profile'
+  get '/explore' => 'pages#explore'
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
