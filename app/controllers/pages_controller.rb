f28187c8e0aec78b738ce73a476da5c7d21a0756
David Allen <davideallen7@gmail.com>
2018-02-16 13:21:31 +1300

    created post controller and form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,13 +3,17 @@ class PagesController < ApplicationController
   end
 
   def home
+    @posts = Post.all
   end
 
   def profile
   	#grab the username from the url as :id
   	@username = params[:id]
+    @newPost = Post.new
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]))
   end
 
   def explore
+    @posts = Post.all
   end
 end
